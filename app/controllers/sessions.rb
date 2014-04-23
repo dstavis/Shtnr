b8cb2c4948c252a75ec32d81413bac0e8a79d96a
Apprentice <apprentice@dbc08.local>
2014-04-23 12:21:32 -0700

    Yo, we added a bunch of things. Added model and migrations for sessions and users.

---
+++ app/controllers/sessions.rb
@@ -0,0 +1,9 @@
+post '/login' do
+  #log in the user IF the credentials they provided match those in the database
+  redirect '/'
+end
+
+# post '/users' do
+#   #create a new user account
+#   redirect '/'
+# end
---
+++ app/controllers/users.rb
@@ -0,0 +1,15 @@
+get '/users' do
+  #get a list of all users
+end
+
+post '/users' do
+  #create a new users
+end
+
+get '/users/:id' do
+  #display an individual users
+end
+
+put '/users/:id' do
+  #update a specific users
+end
