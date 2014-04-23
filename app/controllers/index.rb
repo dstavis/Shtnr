e204d995f393b56f40ae167eeebf1cfd6b82a2fc
Apprentice <apprentice@dbc08.local>
2014-04-23 11:36:12 -0700

    accepts and shortens urls. displays pics of the shat. counts and saves number of click on shortened url.
      *added css
      To Do next release

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,4 +1,22 @@
+require 'securerandom'
+
 get '/' do
   # Look in app/views/index.erb
+  @urls = Url.all
   erb :index
+end
+
+post '/urls' do
+  url = Url.new(params)
+  url.short = SecureRandom.urlsafe_base64(4)
+  url.save
+  redirect '/'
+end
+
+get '/:short_url' do
+  #shtnr/dt4h
+  url = Url.find_by_short(params[:short_url])
+  url.click_count+=1
+  url.save
+  redirect(url.original)
 end
