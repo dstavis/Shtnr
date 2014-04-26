76f79e00a1e666062f000a5dba9b98583c923d1d
David Stavis <davidstavis@gmail.com>
2014-04-25 22:34:11 -0700

    Adds user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,9 @@
 class User < ActiveRecord::Base
   has_many :urls
+
+  validates :email, uniqueness: true
+  validates :email, format: { with: /(\w+)(@)([a-zA-Z]{2,})([.]\w{2,4})/,
+    message: "Must be a valid single-tld email address with no special characters." }
+
+  validates :password, presence: true  	  
 end
